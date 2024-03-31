7af9ffe384ff010fc70907a9c673fdd7a271d471
average-bread <163648686+average-bread@users.noreply.github.com>
2024-04-01 00:40:01 +0200

    Update

--- src/main/java/cursedbread/trashcan/blocks/CompasBlock.java
+++ src/main/java/cursedbread/trashcan/blocks/CompasBlock.java
@@ -16,9 +16,9 @@ public class CompasBlock extends Block {
 
 	public boolean blockActivated(World world, int x, int y, int z, EntityPlayer player) {
 		q = world.getBlockId(x, y, z);
-		i = (int)(Math.random()-10 * 10);
-		j = (int)(Math.random()-10 * 10);
-		k = (int)(Math.random()-10 * 10);
+		i = (int)(Math.random() * 10);
+		j = (int)(Math.random() * 10);
+		k = (int)(Math.random() * 10);
 		world.setBlock(x + i, y + j, z + k, q);
 		return false;
 	}
